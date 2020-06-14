445b57dfb16b0e44ba916f674e5a472afe4af8bc
zhangbowen <zhangbowen@kuaishou.com>
2020-06-15 00:33:00 +0800

    init

---
+++ src/main/java/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhangbowen <zhangbowen@kuaishou.com>
+ * Created on 2020-06-15
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
