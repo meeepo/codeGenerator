30983f0bb4601dbf29d7290b5c478738b7625819
zhangbowen <zhangbowen@kuaishou.com>
2020-09-15 22:49:11 +0800

    1

--- src/main/java/core/util/LogUtil.java
+++ src/main/java/core/util/LogUtil.java
@@ -3,6 +3,8 @@ package core.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
+
 /**
  * @author zhangbowen <zhangbowen@kuaishou.com>
  * Created on 2020-06-18
@@ -15,5 +17,6 @@ public class LogUtil {
         logger.debug("sdf{}", 123);
         logger.warn("sdf{}", 123);
         logger.error("sdf{}", 123);
+        logger.error("sdf{}", Lists.newArrayList(1,2,3));
     }
 }
